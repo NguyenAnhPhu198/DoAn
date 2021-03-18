5719aae1eeb7f4ce3f9706450d5c5a5ad4b16e39
thanhson <thanhson.itnic@gmail.com>
2021-03-18 22:57:32 +0700

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,16 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': ['warn', 2],
+    'no-alert': ['error'],
+    'no-multi-spaces': ['warn', { ignoreEOLComments: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'max-depth': ["error", 4],
+    'max-params': ["error", 5],
+    'no-multiple-empty-lines': "error",
+    'no-trailing-spaces': ["error", { ignoreComments: true }],
+    'no-whitespace-before-property': "error",
   },
   parserOptions: {
     parser: 'babel-eslint'
