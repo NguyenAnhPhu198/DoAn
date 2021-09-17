d42ada6b372cafa5811efe0be2e9779ea137d511
thanhson <thanhson.itnic@gmail.com>
2021-09-17 15:20:00 +0700

    clean Modal

--- src/core/components/T/Modal.vue
+++ src/core/components/T/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <CModal :show="show" :size="size" @update:show="$emit('update:show', $event)">
     <template #header>
-      <TMessage :content="title" bold />
+      <TMessage :content="title" bold capitalize />
       <TButtonClose class="py-0" @click="$emit('update:show', false)" />
     </template>
     <slot></slot>
@@ -18,14 +18,23 @@
 </template>
 
 <script>
-import mixinModal from "./mixinModal";
-
 export default {
-  mixins: [mixinModal],
   props: {
     title: {
       type: String,
       required: true,
+    },
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      required: false,
+    },
+    creating: {
+      type: Boolean,
+      default: false,
     },
   },
 };
