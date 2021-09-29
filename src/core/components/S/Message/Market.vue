32001af3e3be7098e8828684c6b4948372ad9a97
thanhson <thanhson.itnic@gmail.com>
2021-09-29 16:53:51 +0700

    markets

---
+++ src/core/components/S/Message/Market.vue
@@ -0,0 +1,34 @@
+<template>
+  <CHeaderNavLink :to="to" exact class="h-100 market-item mx-2">
+    <CIcon :src="getSrcIcon()" :height="height" />
+    <TMessage :content="name" noTranslate noWrap uppercase class="ml-2" />
+  </CHeaderNavLink>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: Number,
+      default: 20,
+    },
+  },
+  methods: {
+    getSrcIcon() {
+      return `${process.env.VUE_APP_ASSETS_URL}/${this.icon}.png`;
+    },
+  },
+};
+</script>
