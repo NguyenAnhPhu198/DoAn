6b4169ca5d65c667dcbe6556c27cb98c2a8d3cc6
thanhson <thanhson.itnic@gmail.com>
2021-09-30 15:19:38 +0700

    search products

--- src/workspaces/home/views/Home.vue
+++ src/workspaces/home/views/Home.vue
@@ -7,7 +7,7 @@
     </CRow>
     <CRow class="pb-4">
       <CCol>
-        <SearchProducts />
+        <SearchProducts :filters="filters" @search="searchProducts" />
       </CCol>
     </CRow>
     <CRow class="mb-4">
@@ -28,6 +28,21 @@ export default {
     Banner,
     Products,
     SearchProducts,
+  },
+  data() {
+    return {
+      filters: [
+        { value: "name", name: "Name", default: true },
+        { value: "id", name: "Jancode" },
+      ],
+    };
+  },
+  methods: {
+    searchProducts({ filter, key }) {
+      this.$store.dispatch("product.suggest_products.push-query", {
+        search: `${filter.value}:${key}`,
+      });
+    },
   },
 };
 </script>
